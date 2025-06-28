5c7e3b11707f87135c8bf390173e35f7904b21b4
priya.create15 <priys0530@gmail.com>
2025-06-28 13:10:38 +0530

    This is new msg from 2ndweekend

--- ServiceNow/src/test/java/testCases/BaseClass.java
+++ ServiceNow/src/test/java/testCases/BaseClass.java
@@ -1,27 +1,30 @@
 package testCases;
 
+import java.net.URL;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-
+import java.net.MalformedURLException;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 public class BaseClass {
 	public WebDriver driver;
 	public Logger logger;
 	public Properties p;
 	
-	
+
 	@BeforeClass()
 	@Parameters({"os","browser"})
 	public void setup(String os, String br) throws IOException {
@@ -32,13 +35,52 @@ public class BaseClass {
 		
 		logger=LogManager.getLogger(this.getClass());
 		
-		switch(br.toLowerCase()) {
-		case "chrome"	: driver= new ChromeDriver(); break;
-		case "edge"	: driver= new EdgeDriver(); break;
-		case "firefox":driver=new FirefoxDriver();break;
-		default:System.out.println("This is invalid browser");return;
+		//os
+		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+		{
+		DesiredCapabilities cap= new DesiredCapabilities();
 		
+		if(os.equalsIgnoreCase("windows")) {
+			cap.setPlatform(Platform.WIN10);
 		}
+		else if(os.equalsIgnoreCase("Mac")){
+			cap.setPlatform(Platform.MAC);
+			} 
+		else if(os.equalsIgnoreCase("linux")){
+			cap.setPlatform(Platform.LINUX);
+			} 
+		else{
+			System.out.println("Not matching to any platform");
+			return;
+		}
+		
+		//browser
+			switch(br.toLowerCase())	
+			{
+			case "chrome": cap.setBrowserName("chrome"); break;
+			case "firefox": cap.setBrowserName("firefox"); break;
+			case "edge": cap.setBrowserName("edge");break;
+			default: System.out.println("No browsers matching");return;
+			
+			}
+			
+			try {
+			    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+			} catch (MalformedURLException e) {
+			    e.printStackTrace();
+			}
+		}
+			if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+			{
+						
+				switch(br.toLowerCase()) {
+				case "chrome"	: driver= new ChromeDriver(); break;
+				case "edge"	: driver= new EdgeDriver(); break;
+				case "firefox":driver=new FirefoxDriver();break;
+				default:System.out.println("This is invalid browser");return;
+				}
+				
+			}
 		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
